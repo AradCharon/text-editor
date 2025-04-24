0bab3d07654518050c07c74fc326273db229aaee
aradcharon <aradshafiee06@gmail.com>
2025-04-24 21:52:29 +0330

    add

--- src/editor/FileManager.java
+++ src/editor/FileManager.java
@@ -1,29 +1,57 @@
 package editor;
 
 import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 public class FileManager {
     public static void openFile(TextEditor textEditor, JTextArea textArea) {
         JFileChooser fileChooser = new JFileChooser();
         if (fileChooser.showOpenDialog(textEditor) == JFileChooser.APPROVE_OPTION) {
-            //File file = fileChooser.getSelectedFile();
-            //TODO
+            File file = fileChooser.getSelectedFile();
+            try {
+                String content = new String(Files.readAllBytes(file.toPath()));
+                textArea.setText(content);
+                textEditor.currentFile = file;
+            } catch (IOException e) {
+                JOptionPane.showMessageDialog(textEditor,
+                        "Error opening file: " + e.getMessage(),
+                        "Error",
+                        JOptionPane.ERROR_MESSAGE);
+            }
         }
     }
 
     public static void saveFile(TextEditor textEditor, JTextArea textArea) {
-        if (textEditor.currentFile == null){
+        if (textEditor.currentFile == null) {
             JFileChooser fileChooser = new JFileChooser();
             if (fileChooser.showSaveDialog(textEditor) == JFileChooser.APPROVE_OPTION) {
-                //TODO
+                File file = fileChooser.getSelectedFile();
+                try {
+                    Files.write(file.toPath(), textArea.getText().getBytes());
+                    textEditor.currentFile = file;
+                } catch (IOException e) {
+                    JOptionPane.showMessageDialog(textEditor,
+                            "Error saving file: " + e.getMessage(),
+                            "Error",
+                            JOptionPane.ERROR_MESSAGE);
+                }
             }
-        }
-        else{
-            //TODO
+        } else {
+            try {
+                Files.write(textEditor.currentFile.toPath(), textArea.getText().getBytes());
+            } catch (IOException e) {
+                JOptionPane.showMessageDialog(textEditor,
+                        "Error saving file: " + e.getMessage(),
+                        "Error",
+                        JOptionPane.ERROR_MESSAGE);
+            }
         }
     }
 
     public static void newFile(TextEditor textEditor, JTextArea textArea) {
-        //TODO
+        textEditor.currentFile = null;
+        textArea.setText("");
     }
 }
